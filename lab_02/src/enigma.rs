c665c3b8806411f696e4970f3348b332e043867e
Pavel Khetagurov <khetagurow1@yandex.ru>
2021-09-28 13:54:30 +0300

    Add lab_02

--- lab_02/src/enigma.rs
+++ lab_02/src/enigma.rs
@@ -1,70 +1,161 @@
-std::char::{char};
-
+use std::char;
+use rand::prelude::*;
 
 
 pub struct Enigma {
     rotors: Rotors,
-    panel: Panel,
 }
 
 impl Enigma {
     pub fn new() -> Self {
         Self {
             rotors: Rotors::new(),
-            panel: Panel::new(),
         }
     }
 
-    pub fn set_rotors(&self, first: char, second: char, third: char) {
+    pub fn set_rotors(&mut self, first: u8, second: u8, third: u8) {
         self.rotors.set(first, second, third);
     }
 
-    pub fn add_comutate(&self, first: char, second: char) -> bool {
-        self.panel.add_comutate(first, second)
-    }
-
-    pub fn encrypt(&self, message: String) -> String {
-        let encrypted_message = String::default();
+    pub fn encrypt(&mut self, message: String) -> String {
+        let mut encrypted_message = String::default();
         for char_to_en in message.chars() {
-            encrypted_message.add(self.encrypt_letter(char_to_en).to_str());
+            encrypted_message.push(self.encrypt_letter(char_to_en as u8) as char);
         }
         encrypted_message
     }
 
-    fn encrypt_letter(&self, letter: char) -> char {
-        let mut encrypted_letter = self.panel.swap(letter);
-        encrypt_letter = self.rotors.encrypt();
-        self.panel.swap(encrypt_letter)
+    pub fn encrypt_file(&mut self, message: &Vec::<u8>) -> Vec::<u8> {
+        let mut encrypted_message: Vec::<u8> = Vec::new();
+        let mut i = 0;
+        for byte in message {
+            if i % 10000 == 0 {
+                println!("{}", i);
+            }
+            i+=1;
+            encrypted_message.push(self.encrypt_letter(*byte));
+        }
+        encrypted_message
+    }
+
+    fn encrypt_letter(&mut self, letter: u8) -> u8 {
+        self.rotors.encrypt(letter)
     }
 }
 
+#[derive(Debug)]
 pub struct Rotors {
     rotors: Vec<Rotor>,
-    reflector: Rotor,
+    reflector: Reflector,
 }
 
 impl Rotors {
     pub fn new() -> Self {
-        let rotors = Vec::new();
-        rotors.add(Rotor::new());
-        rotors.add(Rotor::new());
-        rotors.add(Rotor::new());
+        let mut rotors = Vec::new();
+        rotors.push(Rotor::new());
+        rotors.push(Rotor::new());
+        rotors.push(Rotor::new());
 
         Self {
             rotors,
-            reflector: Rotor::new(),
+            reflector: Reflector::new(),
         }
     }
 
-    pub fn encrypt(&self, letter: char) -> char {
-        let mut encrypted_letter = letter;
-        for rotor in self.rotors {
-            encrypted_letter = rotor.encrypt(encrypted_letter);
+    pub fn set(&mut self, first: u8, second: u8, third: u8) {
+        self.rotors[0].set(first as u8);
+        self.rotors[1].set(second as u8);
+        self.rotors[2].set(third as u8);
+    }
+
+    pub fn encrypt(&mut self, letter: u8) -> u8 {
+        let mut encrypted_letter = letter as u8;
+
+        for rotor in &self.rotors {
+            encrypted_letter = rotor.forward(encrypted_letter);
         }
-        encrypted_letter = self.reflector.encrypt(encrypted_letter);
-        for rotor in self.rotors.reverse() {
-            encrypted_letter = rotor.encrypt(encrypted_letter);
+        encrypted_letter = self.reflector.reflect(encrypted_letter);
+        self.rotors.reverse();
+        for rotor in &self.rotors {
+            encrypted_letter = rotor.backward(encrypted_letter);
+        }
+        self.rotors.reverse();
+
+        for rotor in &mut self.rotors {
+            if !rotor.click() {
+                break;
+            }
         }
         encrypted_letter
+    }
+}
+
+#[derive(Debug)]
+pub struct Reflector {
+}
+
+impl Reflector {
+    pub fn new() -> Self {
+        Self {
+        }
+    }
+
+    pub fn reflect(&self, letter: u8) -> u8 {
+        if letter % 2 == 0 {
+            letter + 1
+        } else {
+            letter - 1
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct Rotor {
+    vecr: Vec<u8>,
+    start: u8,
+    current: u8,
+}
+
+impl Rotor {
+    pub fn new() -> Self {
+        let mut vecr: Vec<u8> = Vec::new();
+        for i in 0..=255 {
+            vecr.push(i);
+        }
+
+        let mut rng = rand::thread_rng();
+        vecr.shuffle(&mut rng);
+
+        Self {
+            vecr,
+            start: 0,
+            current: 0,
+        }
+    }
+
+    pub fn set(&mut self, start: u8) {
+        self.start = start;
+        self.current = start;
+    }
+
+    pub fn forward(&self, encrypt: u8) -> u8 {
+        self.vecr[(encrypt as usize + self.current as usize) % (u8::MAX as usize + 1)]
+    }
+
+    pub fn backward(&self, encrypt: u8) -> u8 {
+        let mut res = 0;
+        for i in 0..=255 {
+            if self.vecr[(i + self.current as usize) % (u8::MAX as usize + 1)] == encrypt {
+                res = i;
+                break;
+            }
+        }
+        return res as u8;
+
+    }
+
+    pub fn click(&mut self) -> bool {
+        self.current = ((self.current as u32 + 1 as u32) % (u8::MAX as u32 + 1)) as u8;
+        self.current == self.start
     }
 }
--- lab_02/src/main.rs
+++ lab_02/src/main.rs
@@ -1,3 +1,114 @@
+use std::io::stdin;
+use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
+
+mod enigma;
+use enigma::Enigma;
+
 fn main() {
-    println!("Hello, world!");
+    let mut choose = 1;
+    let mut buf = String::default();
+    let mut enigma = Enigma::new();
+    while choose != 0 {
+        print_menu();
+        stdin().read_line(&mut buf).expect("Expected choose");
+        let res = buf.trim();
+        buf = String::from(res);
+        println!("o{}o", buf);
+        let res_choose = buf.parse();
+        if let Err(res) = res_choose {
+            println!("{} Expected choose. Try again\n", res);
+            buf = String::default();
+            continue;
+        }
+
+        choose = res_choose.unwrap();
+        match choose {
+            1 => call_set_rotors(&mut enigma),
+            2 => encrypt(&mut enigma),
+            3 => encrypt_file(&mut enigma),
+            _ => (),
+        }
+        println!("\n");
+        buf = String::default();
+    }
+}
+
+fn print_menu() {
+    println!("0. Exit");
+    println!("1. Set rotors");
+    println!("2. Encrypt string");
+    println!("3. Encrypt file");
+}
+
+fn call_set_rotors(enigma: &mut Enigma) {
+    let mut buf = String::default();
+
+    println!("Insert start symbol for rotor 1: ");
+    stdin().read_line(&mut buf).expect("Expected choose");
+    let res = buf.trim();
+    buf = String::from(res);
+    let chars = buf.chars();
+    let first = chars.collect::<Vec<char>>()[0];
+
+    let mut buf = String::default();
+    println!("Insert start symbol for rotor 2: ");
+    stdin().read_line(&mut buf).expect("Expected choose");
+    let res = buf.trim();
+    buf = String::from(res);
+    let chars = buf.chars();
+    let second = chars.collect::<Vec<char>>()[0];
+
+    let mut buf = String::default();
+    println!("Insert start symbol for rotor 3: ");
+    stdin().read_line(&mut buf).expect("Expected choose");
+    let res = buf.trim();
+    buf = String::from(res);
+    let chars = buf.chars();
+    let third = chars.collect::<Vec<char>>()[0];
+
+    enigma.set_rotors(first as u8, second as u8, third as u8);
+}
+
+fn encrypt(enigma: &mut Enigma) {
+    let mut buf = String::default();
+    println!("Insert line for encrypt: ");
+    stdin().read_line(&mut buf).expect("Expected data");
+    let res = buf.trim();
+    buf = String::from(res);
+    let encrypted = enigma.encrypt(buf);
+    println!("Encrypted: {}", encrypted);
+}
+
+fn encrypt_file(enigma: &mut Enigma) {
+    let mut buf = String::default();
+    println!("Insert file for encrypt: ");
+    stdin().read_line(&mut buf).expect("Expected data");
+    let res = buf.trim();
+    let path = Path::new(res);
+    let display = path.display();
+
+    let mut encr_buf = String::from(res);
+    encr_buf.push_str("_encr");
+    
+
+    // Open the path in read-only mode, returns `io::Result<File>`
+    let mut file = match File::open(&path) {
+        Err(why) => {println!("couldn't open {}: {}", display, why); return}
+        Ok(file) => file,
+    };
+    // Read the file contents into a string, returns `io::Result<usize>`
+    let mut buffer = Vec::<u8>::new();
+    if let Ok(size) = file.read_to_end(&mut buffer) {
+        println!("Read {} bytes", size);
+    } else {
+        println!("Error in read");
+        return
+    }
+    let encr_content = enigma.encrypt_file(&buffer);
+    //println!("{}", encr_content);
+    let mut new_file = File::create(Path::new(&encr_buf)).expect("Error in create encrypted file");
+    new_file.write_all(encr_content.as_ref());
+    new_file.sync_all();
 }
